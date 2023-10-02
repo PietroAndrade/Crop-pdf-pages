4f330f5b095cdfa31934f554835f005f6887e2d9
PietroAndrade <76005839+PietroAndrade@users.noreply.github.com>
2023-10-02 18:37:00 -0300

    feat(general sizes): method for generalising cut sizes

--- splitpages.py
+++ splitpages.py
@@ -3,18 +3,52 @@ from PyPDF2 import PdfWriter, PdfReader
 reader = PdfReader("example.pdf")
 writer = PdfWriter()
 
-lowerLeft = 842/2
+#Variables
+y_upper_right = 0
+x_upper_right = 0
+y0_lower_left = 0
+x0_lower_left = 0
 
+lowerLeft = 0
+
+#Methods
 def crop_sides(page_to_crop, side='right'):
 
     if(side == 'right'):
-        page_to_crop.cropbox.lower_left = (0, 0)
-        page_to_crop.cropbox.upper_right = (595.22, lowerLeft)
+        page_to_crop.cropbox.lower_left = (y0_lower_left, x0_lower_left)
+        page_to_crop.cropbox.upper_right = (y_upper_right, lowerLeft)
         writer.add_page(page_to_crop)
     elif(side == 'left'):
-        page_to_crop.cropbox.lower_left = (0, lowerLeft)
-        page_to_crop.cropbox.upper_right = (595.22, 842)
+        page_to_crop.cropbox.lower_left = (y0_lower_left, lowerLeft)
+        page_to_crop.cropbox.upper_right = (y_upper_right, x_upper_right)
         writer.add_page(page_to_crop)
+
+def eval_sizes(page_to_eval):
+    yx = page_to_eval.cropbox.upper_right
+    y_upper_right = yx[0]
+    x_upper_right = yx[1]
+    print(f'{y_upper_right},{x_upper_right}')
+
+    x0y = page_to_eval.cropbox.lower_right
+    y_lower_right = x0y[0]
+    x0_lower_right = x0y[1]
+    print(f'{y_lower_right},{x0_lower_right}')
+
+    xy0 = page_to_eval.cropbox.upper_left
+    y0_upper_left = xy0[0]
+    x_upper_left = xy0[1]
+    print(f'{y0_upper_left},{x_upper_left}')
+
+    xy00 = page_to_eval.cropbox.lower_left
+    y0_lower_left = xy00[0]
+    x0_lower_left = xy00[1]
+    print(f'{y0_lower_left},{x0_lower_left}')
+
+    lowerLeft = x_upper_right/2
+    print(f'Half:{lowerLeft}')
+
+#Executing
+eval_sizes(reader.pages[1])
 
 for each_page in reader.pages:
     crop_sides(each_page, 'right')
